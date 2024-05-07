536bbcc44cb318504051cbca65b4676c6a3b2831
iszm13 <ishtveersinghbilling@gmail.com>
2024-05-08 03:56:17 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import base64
 import joblib
+import altair as alt
+from PIL import Image
 
 ######################
 # Stock Prices Project
@@ -14,7 +16,7 @@ import joblib
 
 def display_stock_data(company):
     st.write(f"### Stock Prices for {company}")
-    data = yf.download(company, start="2020-01-01", end="2024-01-01")
+    data = yf.download(company, start="2000-01-01", end="2024-01-01")
     st.line_chart(data[['Close', 'Volume']])
 
 ######################
@@ -152,7 +154,7 @@ def main():
     st.title('Combined Web App CO21325')
 
     st.write("""
-    # Combined Web App
+
     
     This app combines multiple projects.
     
@@ -193,6 +195,34 @@ def main():
         sequence
 
         display_DNA_nucleotide_count(sequence)
+        X = DNA_nucleotide_count(sequence)
+        
+        ### 2. Print text
+        st.subheader('2. Print text')
+        st.write('There are  ' + str(X['A']) + ' adenine (A)')
+        st.write('There are  ' + str(X['T']) + ' thymine (T)')
+        st.write('There are  ' + str(X['G']) + ' guanine (G)')
+        st.write('There are  ' + str(X['C']) + ' cytosine (C)')
+
+        ### 3. Display DataFrame
+        st.subheader('3. Display DataFrame')
+        df = pd.DataFrame.from_dict(X, orient='index')
+        df = df.rename({0: 'count'}, axis='columns')
+        df.reset_index(inplace=True)
+        df = df.rename(columns = {'index':'nucleotide'})
+        st.write(df)
+
+        ### 4. Display Bar Chart using Altair
+        st.subheader('4. Display Bar chart')
+        p = alt.Chart(df).mark_bar().encode(
+            x='nucleotide',
+            y='count'
+        )
+        p = p.properties(
+            width=alt.Step(80)  # controls width of bar.
+        )
+        st.write(p)
+
 
     elif selected_project == "EDA Basketball":
         st.write("""
